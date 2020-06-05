3a4668a33cea2a891546f11ba2e9363dc4678133
setyoawan <setyoawan8@gmail.com>
2020-06-05 10:30:33 +0700

    Updates

---
+++ precache-manifest.0d9f526dacbddf6856d45a6a2740690d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48228b3ec008e21d2dab6f2da86b07a",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "8c7c0a223a87b054d480",
+    "url": "/site/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "90e6716777c182df81d5",
+    "url": "/site/static/css/main.ae471f10.chunk.css"
+  },
+  {
+    "revision": "8c7c0a223a87b054d480",
+    "url": "/site/static/js/2.eda23c15.chunk.js"
+  },
+  {
+    "revision": "a8f962b213a67202a0ddc78ab1211cdb",
+    "url": "/site/static/js/2.eda23c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e6716777c182df81d5",
+    "url": "/site/static/js/main.1569cbf1.chunk.js"
+  },
+  {
+    "revision": "5c15a8345eacfa8093a3",
+    "url": "/site/static/js/runtime-main.2778bf1e.js"
+  },
+  {
+    "revision": "90e5b4d764b236e7c637ef4a46e9849f",
+    "url": "/site/static/media/awan.90e5b4d7.png"
+  },
+  {
+    "revision": "dc10e1e858df489227ceb2d852bbc0df",
+    "url": "/site/static/media/bangkalan.dc10e1e8.png"
+  },
+  {
+    "revision": "23b25ff349b2bd4c0b537eed71da3e55",
+    "url": "/site/static/media/bg-img3.23b25ff3.jpg"
+  },
+  {
+    "revision": "7944590a718bfb68806a64de8b4be729",
+    "url": "/site/static/media/bwa.7944590a.png"
+  },
+  {
+    "revision": "41a3e1c9da08f1a3f0a9716c4280a8be",
+    "url": "/site/static/media/cisco.41a3e1c9.png"
+  },
+  {
+    "revision": "c13b8fef706058966349be06422f25d0",
+    "url": "/site/static/media/home-banner.c13b8fef.jpg"
+  },
+  {
+    "revision": "2b78beaf84dabe8a1e13f54af19a4f35",
+    "url": "/site/static/media/movie-sap.2b78beaf.png"
+  },
+  {
+    "revision": "82228ccaf79ad4ca20458e28f0e1ec8a",
+    "url": "/site/static/media/setyo.82228cca.jpg"
+  },
+  {
+    "revision": "9c2e8c84f45449973830388b31093742",
+    "url": "/site/static/media/socah.9c2e8c84.png"
+  },
+  {
+    "revision": "929ffc8b26795c870cc4630b74c331be",
+    "url": "/site/static/media/utm.929ffc8b.png"
+  },
+  {
+    "revision": "91239cc82e9ab527e8ad9853c8f66a21",
+    "url": "/site/static/media/workhard.91239cc8.jpg"
+  }
+]);
